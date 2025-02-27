50cd6f03a1dacbcd2518ae7761c23cf2a00981da
Алексей Розеган <rozeganalexey@gmail.com>
2025-02-27 18:23:10 +0100

    Solution

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM python:3.11
+LABEL authors="roleksii"
+
+ENV PYTHONNBUFFERED=1
+
+WORKDIR app/
+
+COPY requirements.txt requirements.txt
+RUN  pip install -r requirements.txt
+
+COPY . .
+
+
+CMD ["python", "app/main.py"]
--- app/main.py
+++ app/main.py
@@ -1,7 +1,34 @@
-def get_weather() -> None:
-    # write your code here
-    pass
+import os
+import requests
+
+
+API_KEY = os.getenv("API_KEY")
+BASE_URL = "http://api.weatherapi.com/v1/current.json?"
+CITY = os.getenv("CITY", "Paris")
+
+
+def get_weather(api_key, city):
+    url = "http://api.weatherapi.com/v1/current.json"
+    params = {
+        "key": api_key,
+        "q": city,
+        "aqi": "no"
+    }
+
+    try:
+        response = requests.get(url, params=params)
+        response.raise_for_status()
+        data = response.json()
+        condition = data["current"]["condition"]["text"]
+        temp_c = data["current"]["temp_c"]
+        time = data["location"]["localtime"]
+        print(f"Weather in {city}: {condition}, {temp_c}°C (Local Time: {time})")
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching weather data: {e}")
 
 
 if __name__ == "__main__":
-    get_weather()
+    if not API_KEY:
+        print("API_KEY environment variable is required.")
+    else:
+        get_weather(API_KEY, CITY)
